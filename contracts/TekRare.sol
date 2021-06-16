6bdd2b3786db443a43acbaa10de88c7aec99f961
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-06-17 01:22:06 +0200

    feat(TekRare): replace usual ERC1155 inheritance by the totalSupply one

--- contracts/TekRare.sol
+++ contracts/TekRare.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "./ERC1155Supply.sol";
 import "./AccessControl.sol";
 import "./Payable.sol";
 
-contract TekRare is ERC1155, AccessControl, Payable {
+contract TekRare is ERC1155Supply, AccessControl, Payable {
   constructor (string memory uri) ERC1155(uri) {}
 }
