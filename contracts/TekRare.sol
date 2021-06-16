690918dd795ca7257fbace575e8125ef54116846
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-06-17 01:23:46 +0200

    feat(TekRare): implement setUri, mint and mintBatch public functions

--- contracts/TekRare.sol
+++ contracts/TekRare.sol
@@ -8,4 +8,16 @@ import "./Payable.sol";
 
 contract TekRare is ERC1155Supply, AccessControl, Payable {
   constructor (string memory uri) ERC1155(uri) {}
+
+  function setUri(string memory uri) public onlyAdmin {
+    _setURI(uri);
+  }
+
+  function mint(address to, uint256 id, uint256 amount, bytes memory data) public onlyAdmin {
+    _mint(to, id, amount, data);
+  }
+
+  function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyAdmin {
+    _mintBatch(to, ids, amounts, data);
+  }
 }
