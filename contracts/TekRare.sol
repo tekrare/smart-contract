adb9165f9a814485af0ad832e53d4a3a2cbab1c7
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-06-18 00:09:21 +0200

    feat(TekRare): use ITekRare interface and override functions

--- contracts/TekRare.sol
+++ contracts/TekRare.sol
@@ -5,19 +5,20 @@ pragma solidity ^0.8.0;
 import "./ERC1155Supply.sol";
 import "./AccessControl.sol";
 import "./Payable.sol";
+import "./ITekRare.sol";
 
-contract TekRare is ERC1155Supply, AccessControl, Payable {
+contract TekRare is ITekRare, ERC1155Supply, AccessControl, Payable {
   constructor (string memory uri) ERC1155(uri) {}
 
-  function setUri(string memory uri) public onlyAdmin {
+  function setUri(string memory uri) public override onlyAdmin {
     _setURI(uri);
   }
 
-  function mint(address to, uint256 id, uint256 amount, bytes memory data) public onlyAdmin {
+  function mint(address to, uint256 id, uint256 amount, bytes memory data) public override onlyAdmin {
     _mint(to, id, amount, data);
   }
 
-  function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyAdmin {
+  function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public override onlyAdmin {
     _mintBatch(to, ids, amounts, data);
   }
 }
