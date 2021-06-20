74931780a132fb56e393481861f18c5fd31eef87
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-06-20 20:50:57 +0200

    style: add IAuctionManager inheritance to ITekRare interface

--- contracts/ITekRare.sol
+++ contracts/ITekRare.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "./IAuctionManager.sol";
 
-interface ITekRare is IERC1155 {
+interface ITekRare is IERC1155, IAuctionManager {
   function tokenAmount() external view returns (uint);
   function setUri(string memory uri) external;
   function mint(address to, uint tokenId, uint amount) external;
